77e5c9968af8d69bc8cb693fe5ef0616c4f6b206
CodeGPTHub <139706396+CodeGPTHub@users.noreply.github.com>
2023-07-16 20:46:44 +0300

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+// Creating type aliases
+type Point = { x: number, y: number };
+type Shape = "circle" | "square" | "triangle";
+
+// Using type aliases and union types
+function drawShape(shape: Shape): void {
+  // ...
+}
+
+function calculateDistance(point: Point): number {
+  // ...
+}
